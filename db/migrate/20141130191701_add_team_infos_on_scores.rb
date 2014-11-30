1a8f77cb12910c7f741b6b4f0098e1f959fc1c6c
Alessandro Tegner <alessandro.tegner@gmail.com>
2014-11-30 22:01:15 +0000

    Fix

--- db/migrate/20141130191701_add_team_infos_on_scores.rb
+++ db/migrate/20141130191701_add_team_infos_on_scores.rb
@@ -3,12 +3,6 @@ class AddTeamInfosOnScores < ActiveRecord::Migration
     add_column :scores, :team_name, :string
     add_column :scores, :team_logo, :string
 
-    Score.find_each do |s|
-      s.team_name = s.player.acronym || s.player.email
-      s.team_logo = "http://www.gravatar.com/avatar/#{s.player.email_md5}?s=20&d=wavatar"
-      s.save!
-    end
-
     change_column_null :scores, :team_name, false
   end
 end
