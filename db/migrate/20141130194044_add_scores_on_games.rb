b860d4748fc1907ba8fad2d0cf0cb0072e82066d
Alessandro Tegner <alessandro.tegner@gmail.com>
2014-11-30 22:07:33 +0000

    Fix

--- db/migrate/20141130194044_add_scores_on_games.rb
+++ db/migrate/20141130194044_add_scores_on_games.rb
@@ -3,6 +3,16 @@ class AddScoresOnGames < ActiveRecord::Migration
     add_column :games, :home_score_id, :integer
     add_column :games, :away_score_id, :integer
 
+    Game.find_each do |g|
+      home_score = Score.find_by player_id: g.home_player_id
+      away_score = Score.find_by player_id: g.away_player_id
+
+      g.home_score_id = home_score.id
+      g.away_score_id = away_score.id
+
+      g.save!
+    end
+
     change_column_null :games, :home_score_id, false
     change_column_null :games, :away_score_id, false
 
